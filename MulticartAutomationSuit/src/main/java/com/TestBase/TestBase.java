5651b2f2c3eb4ccfb4f8c8436d12135ee64dd723
khosr <khosr@DESKTOP-A7ACS09.lan>
2021-12-22 13:04:29 -0500

    Chaptar One

---
+++ MulticartAutomationSuit/src/main/java/com/TestBase/TestBase.java
@@ -0,0 +1,63 @@
+package com.TestBase;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import com.utill.Utility;
+
+public class TestBase {
+	public static WebDriver driver;
+	public static Properties prop;
+	public static String conProp = "C:\\Users\\khosr\\eclipse-workspace\\MulticartAutomationSuit\\src\\main\\java\\com"
+			+ "\\configurations\\config.properties";
+	
+	// Call to constractor
+	public TestBase() {
+		prop = new Properties();
+		try {
+			FileInputStream fis = new FileInputStream(conProp);
+			prop.load(fis);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+	
+	public static void initialization() {
+		String browserName = prop.getProperty("browser");
+		if(browserName.equals("chrom")) {
+			System.setProperty("webdriver.chrome.driver","C:\\Users\\khosr\\Driver\\chromedriver.exe");	
+			driver = new ChromeDriver();
+		} else if(browserName.equals("firefox")) {
+			System.setProperty("webdriver.gecko.driver", "C:\\Users\\khosr\\Driver\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		}else {
+			driver = new SafariDriver();
+		}
+		
+		driver.manage().deleteAllCookies();
+		driver.navigate().refresh();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(Utility.PAGE_LOAD_TIME_OUT, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(Utility.IMPLECITELY_WAIT_TIME, TimeUnit.SECONDS);
+		driver.get(prop.getProperty("url"));
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
---
+++ MulticartAutomationSuit/src/main/java/com/utill/Utility.java
@@ -0,0 +1,9 @@
+package com.utill;
+
+import com.TestBase.TestBase;
+
+public class Utility extends TestBase{
+	public static long PAGE_LOAD_TIME_OUT = 20;
+	public static long IMPLECITELY_WAIT_TIME = 20;
+
+}
